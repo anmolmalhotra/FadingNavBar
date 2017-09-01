db9573033aae9e709387f539b4efd21104660a1e
Anmol Malhotra <anmol.github@gmail.com>
2017-09-01 15:49:39 +0530

    Improved code for smoother animations.

--- NavBar/ViewController.swift
+++ NavBar/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
- let offsetThreshold:CGFloat = 50.0
+    
+    let offsetThreshold:CGFloat = 64.0
+    
     let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         return tableView
@@ -18,13 +20,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func setNavBarTransparent() {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
-        UIApplication.shared.statusBarStyle = .lightContent
     }
     
     func updateNavBarOpacity(alpha: CGFloat) {
-        let navImage = UIImage(color: UIColor(colorLiteralRed: 1.0, green: 1.0, blue: 1.0, alpha: Float(alpha)), size: CGSize(width: self.view.frame.width, height: 64))
+        let navImage = UIImage(color: UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: alpha), size: CGSize(width: self.view.frame.width, height: 64))
         self.navigationController?.navigationBar.setBackgroundImage(navImage, for: .default)
-        self.navigationItem.title = "Home"
         UIApplication.shared.statusBarStyle = .default
     }
     
@@ -50,11 +50,32 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print(scrollView.contentOffset.y)
         
         let offset = scrollView.contentOffset.y
-        updateNavBarOpacity(alpha: offset/offsetThreshold)
+        let alphaOffset = offset/offsetThreshold
         
+        if alphaOffset > 1.0 && alphaOffset < 2.5 {
+            let fadeTextAnimation = CATransition()
+            fadeTextAnimation.duration = 0.5
+            fadeTextAnimation.type = kCATransitionFade
+            
+            self.navigationController?.navigationBar.layer.add(fadeTextAnimation, forKey: "fadeText")
+            self.navigationItem.title = "Home"
+        } else if alphaOffset < 1.0 {
+            self.navigationItem.title = nil
+        }
+        
+        if alphaOffset > 0.0 {
+            updateNavBarOpacity(alpha: alphaOffset)
+        } else {
+            setNavBarTransparent()
+        }
+        
+        if alphaOffset > 0.4 {
+            self.navigationController?.navigationBar.shadowImage = nil
+        } else {
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -63,12 +84,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = UIView()
-        header.backgroundColor = .red
+        
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Office"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        
+        header.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.topAnchor.constraint(equalTo: header.topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: header.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: header.rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: header.bottomAnchor).isActive = true
+        
         return header
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return view.frame.width / 2
+        return view.frame.width / 1.5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
